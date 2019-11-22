f1493836e570e04f171973a7e7615584d41f5ed3
Joao Paulo Back <joaoback47@gmail.com>
2019-11-22 08:24:10 -0200

    log

--- app/CreateApp/index.php
+++ app/CreateApp/index.php
@@ -16,17 +16,27 @@
   error_reporting(0); // Disable all errors.
 
   // ORDEM DAS PASTAS QUE SERÃO CRIADOS
+  print_r("\n==============================================\n");
+  print_r("-> EXTRAINDO INFORMAÇÕES DO SQL...\n");
   (new BuildJson('../CreateControllers/test.sql'))->getJson();
+  print_r("-> CRIANDO CLASSES CONTROLLERS...\n");
   (new ControllerBuilder('results.json'))->createControllerClass();
-  (new ControllerBuilder('results.json'))->createControllerClass();
+  print_r("-> CRIANDO CLASSES BO's...\n");
   (new BuildBo('results.json'))->createBoObjects();
+  print_r("-> CRIANDO CLASSES DAO's...\n");
   (new BuildDao('results.json'))->createDaoObjects();
+  print_r("-> CRIANDO CLASSES DTO's...\n");
   (new BuildDto('results.json'))->createDtoObjects();
+  print_r("-> CRIANDO ARQUIVO DE ROTAS...\n");
   (new RouteBuilder('results.json'))->createRoutesFile();
+  print_r("-> CRIANDO VIEWS...\n");
   (new ViewBuilder())->createViews();
+  print_r("-> CRIANDO PASTA PÚBLICA...\n");
   (new PublicBuilder())->createPublic();
+  print_r("-> ATUALIZANDO AUTOLOAD NA PASTA DO PROJETO...\n");
   exec("cd ../../project/ && composer update");
   exec("cd ../../project/ && composer dump-autoload -o");
+  print_r("-> GERANDO PASTA CORE NA RAIZ DO PROJETO...\n");
   exec("cd ../../project/ && git clone https://joaoback47@bitbucket.org/joaoback47/core.git");
-  
+  print_r("==============================================");
 ?>
