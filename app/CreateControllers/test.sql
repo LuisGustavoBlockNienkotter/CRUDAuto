7412a0aa737e4a8dd0c178968f252d5fdee72a93
João Paulo Back <joaoback47@gmail.com>
2019-11-27 00:32:58 -0300

    arrumando header e listar

--- app/CreateControllers/test.sql
+++ app/CreateControllers/test.sql
@@ -3,7 +3,7 @@ create database project01;
 use project01;
 
 create table usuario (
-  id int not null,
+  id int not null auto_increment,
   nome varchar(50) not null,
   login varchar(50) not null,
   senha varchar(50) not null,
@@ -11,7 +11,7 @@ create table usuario (
 );
 
 create table produto (
-  id int not null,
+  id int not null auto_increment,
   descricao varchar(50) not null,
   ncm varchar(50) not null,
   estoque int not null,
@@ -19,7 +19,7 @@ create table produto (
 );
 
 create table fornecedor (
-  id int not null,
+  id int not null auto_increment,
   nome varchar(50) not null,
   cpf varchar(50) not null,
   primary key(id)
@@ -32,5 +32,4 @@ create table rocket (
   largura float not null,
   peso float not null,
   primary key (id)
-)
-
+);
--- project/app/interfaces/IDAO.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\interfaces; 
-interface IDAO{
-	public function insert($object);
-	public function findAll();
-	public function update($object);
-	public function delete($object);
-}
-?>
